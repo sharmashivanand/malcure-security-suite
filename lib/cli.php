412f3f875df88de7dcde2783984fcf4cf2c4e272
Shivanand Sharma <shivanand@binaryturf.com>
2023-08-08 17:28:10 +0530

    troubleshooting incomplete indexing

--- lib/cli.php
+++ lib/cli.php
@@ -4,31 +4,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
 	class MSS_CLI {
-		function data( $args, $assoc_args ) {
-			global $wpdb;
-			// $table  = $wpdb->prefix . 'mss_checksums';
-			// $delete = $wpdb->query( "TRUNCATE TABLE $table" );
-			// $table  = $wpdb->prefix . 'mss_files';
-			// $delete = $wpdb->query( "TRUNCATE TABLE $table" );
-
-			$starttime = new DateTime( 'now' );
-			$opt       = Malcure_Malware_Scanner::get_instance();
-			// $opt->index_files_recursively( WP_PLUGIN_DIR );
-			$opt->get_unserialized_data();
-			// $opt       = mss_utils::update_checksums_themes();
-			// $opt = mss_utils::update_checksums_plugins();
-			// krsort( $opt );
-			// WP_CLI::log( print_r( $opt, 1 ) );
-			$endtime        = new DateTime( 'now' );
-			$interval       = $endtime->diff( $starttime );
-			$elapsedHours   = $interval->h; // Elapsed hours
-			$elapsedMinutes = $interval->i; // Elapsed minutes
-			$elapsedSeconds = $interval->s; // Elapsed seconds
-
-			$elapsedTime = sprintf( '%02d:%02d:%02d', $elapsedHours, $elapsedMinutes, $elapsedSeconds );
-
-			echo "Elapsed time: $elapsedTime";
+		function test( $args, $assoc_args ) {
+			WP_CLI::log( $_SERVER['REQUEST_TIME'] );
 		}
+
 		function dump( $args, $assoc_args ) {
 			global $wpdb;
 			// $table  = $wpdb->prefix . 'mss_checksums';
@@ -37,12 +16,14 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 			// $delete = $wpdb->query( "TRUNCATE TABLE $table" );
 
 			$starttime = new DateTime( 'now' );
-			$opt       = Malcure_Malware_Scanner::get_instance();
-			$opt->start_scanning( ABSPATH );
-			$opt->get_unserialized_data();
-			// $opt->get_unserialized_data();
-			// $opt       = mss_utils::update_checksums_themes();
-			// $opt = mss_utils::update_checksums_plugins();
+			$opt = wp_remote_get(
+				admin_url( 'admin-ajax.php?action=mss_start_scan' ),
+				array(
+					'timeout'  => 0.01,
+					'blocking' => false,
+				)
+			);
+
 			// krsort( $opt );
 			// WP_CLI::log( print_r( $opt, 1 ) );
 			$endtime        = new DateTime( 'now' );
